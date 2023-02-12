687fbbe36a9eb68949ee3d4e45ed5b3406fd59ee
EidanU <eidanughetti.contact@gmail.com>
2023-02-12 20:21:30 +0100

    [WIP] add checking if android or apple

--- src/pages/CallPage.tsx
+++ src/pages/CallPage.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable import/no-extraneous-dependencies */
-import { View } from 'react-native';
+import { Platform, Text, View } from 'react-native';
 import React, { useEffect, useRef, useState } from 'react';
 import { Button } from 'react-native-paper';
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -9,6 +9,7 @@ import { Audio } from 'expo-av';
 function CallPage({ navigation }) {
   const [sound, setSound] = useState();
   const [isplaying, setIsplaying] = useState();
+  const deviceOS = Platform.OS;
 
   async function handleSound() {
     if (isplaying) {
@@ -29,6 +30,10 @@ function CallPage({ navigation }) {
     }
     : undefined), [sound]);
 
+  useEffect(() => {
+    Platform.OS === 'ios' ? console.log('Device is IOS') : console.log('Device is Android');
+  }, []);
+
   return (
     <View>
       <Button mode="contained" onPress={() => handleSound()}>{isplaying ? 'Pause' : 'Play'}</Button>
@@ -39,6 +44,9 @@ function CallPage({ navigation }) {
       >
         back
       </Button>
+      <Text>
+        {deviceOS === 'ios' ? 'IOS' : 'Android'}
+      </Text>
     </View>
   );
 }
