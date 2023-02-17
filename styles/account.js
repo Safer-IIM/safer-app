724e393997a16a3d95d99314c8a1c89a312f026f
EidanU <eidanughetti.contact@gmail.com>
2023-02-17 09:50:28 +0100

    [DISPLAY] change account background color

--- styles/account.js
+++ styles/account.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   accountContainer: {
-    backgroundColor: 'green',
+
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
