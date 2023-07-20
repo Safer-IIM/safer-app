46ef3d9263b218f65d29f48206684903d34157cb
EidanU <eidanughetti.contact@gmail.com>
2023-07-20 16:33:22 +0200

    remove phone number

--- styles/contact.js
+++ styles/contact.js
@@ -1,35 +1,36 @@
-import { StyleSheet } from 'react-native';
-import globalStyle from './variables';
+import { StyleSheet } from "react-native";
+import globalStyle from "./variables";
 
 const styles = StyleSheet.create({
   contactContainer: {
-    position: 'relative',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: "relative",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 10,
-    maxHeight: '95%',
+    maxHeight: "95%",
   },
   listContainer: {
-    width: '100%',
-    height: 'auto',
+    width: "100%",
+    height: "auto",
   },
   item: {
     marginTop: 10,
     padding: 10,
     backgroundColor: globalStyle.colorPrimarySemiVisible,
     borderRadius: 30,
+    justifyContent: "center",
   },
   itemMail: {
-    marginBottom: 10,
-    flexDirection: 'row',
+    alignItems: "center",
+    flexDirection: "row",
   },
   itemPhone: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   addContactButtonContainer: {
     marginTop: 10,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   addContactButton: {
     color: globalStyle.colorPrimary,
